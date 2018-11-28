539a45584d568861a2669ce1011962b3738b689d
programer10 <programer10@ucione.local>
2018-11-28 21:29:20 +0100

    Add ajax request

--- src/js/app.js
+++ src/js/app.js
@@ -1,44 +1,43 @@
 var handleContent = function(){
-	var contentDiv = '<div id = "content">';
-	var clockDiv = '<div id = "clock">';
-	var slider = '<input type = "range" id = "slider" value = "100" max = "150" min = "50">';
-	
-	$('body').prepend(contentDiv);
-	$('#content')
-		.addClass('content')
-		.append(slider)
-		.append(clockDiv); //dodaje element na kraj , prepend dodaje na početak
-	$('#clock').addClass('clock');
-	
-	var mouseDown = false;
-	$('#slider')
-	    .mousedown(function(){
-			mouseDown = true;
-		})
-		.mouseup(function(){
-			mouseDown = false;
-		})
-		.mousemove(function(){
-			if(mouseDown){
-				$('#clock').css('font-size', $(this).val() + 'px');
+	var preloader = $('.preloader');
+	var ajaxLoader = '<img class = "ajax-loader" src = "/img/ajax-loader.gif" />';
+	$('.btn').click(function(e){
+		e.preventDefault();
+		
+		$.ajax({
+			url: "data/users.json",
+			type: "GET",
+			data: {
+				"id" : 1
+			},
+			beforeSend: function(){
+				preloader.empty();
+				preloader.append(ajaxLoader);
+			},		
+			success: function(result, status, xhr){
+				/* Ponekad je potrebno parsirati JSON koji dobijemo u rezultatu u JS objekt*/
+				// var users = $.parseJSON(result);
+				// console.log(users);
+			},
+			error: function(xhr, status, error){
+				if(error){
+					preloader.empty();
+					preloader.text("An error occured while proccessing your request! Please try again later!");
+					setTimeout(function(){
+						preloader.empty();
+					}, 5000);
+				}
+			},
+			complete: function(){
+				preloader.empty();
 			}
 		});
-	
-	runClock();
-	var interval = setInterval(runClock, 1000);
-	
-	function runClock(){
-		var date = new Date();
-		var hh = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours();
-		var mm = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes();
-		var ss = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
-		$('#clock').text(hh + ':' + mm + ':' + ss);
-	}
+	});
 };
 
 var App = function(){
 	return {
-		init: function(element){
+		init: function(){
 			handleContent();
 		}
 	}
